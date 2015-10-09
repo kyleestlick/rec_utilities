e636597933e1706c294001e0a3dfcb62ed835e6c
Ian Wesley-Smith <iwsmith@gmail.com>
2015-10-09 12:09:31 -0700

    Centrality, this time with no networkx

--- babel_util/recommenders/centrality.py
+++ babel_util/recommenders/centrality.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 import networkx as nx
+from collections import defaultdict
 
-def centrality(G):
-    total_in_degree = 0.0
-    for _, in_degree in G.in_degree_iter():
-        total_in_degree += in_degree
 
-    for node_id, in_degree in G.in_degree_iter():
-        yield (node_id, in_degree/total_in_degree)
+class Centrality:
+    def __init__(self):
+        self.nodes = defaultdict(int)
+        self.edge_count = 0.0
+
+    def parse_file(self, stream, delimiter='\t'):
+        for from_id, to_id in stream.split(delimiter):
+            self.nodes[to_id] += 1
+            self.edge_count += 1
+
+        for node_id, count in self.nodes.iteritems():
+            yield node_id, count/self.edge_count
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('infile')
-    parser.add_argument('-d', '--delimiter', default='/t')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-d', '--delimiter', default='\t')
 
     args = parser.parse_args()
-    G = nx.read_edgelist(args.infile, args.delimiter, create_using=nx.DiGraph)
+    centrality = Centrality()
 
-    for node_id, score in centrality(G):
+    for node_id, score in centrality.parse_file(args.infile):
         args.outfile.write("{0}\t{1}\n".format(node_id, score))
-    print(nx.info(G))
