a6b5f6f19511e0d2b1121a3feefbfdfdc176303c
Ian Wesley-Smith <iwsmith@gmail.com>
2015-10-19 22:08:32 -0700

    Add specific MAS file parsers

--- babel_util/parsers/mas.py
+++ babel_util/parsers/mas.py
@@ -28,7 +28,53 @@ def parse_line_tuple(line, delimiter='\t'):
     return map(str.strip, line.split(delimiter))
 
 
-class MASParser:
+class MASAbstract(object):
+    def __init__(self, stream, delimiter="\t"):
+        self._stream = stream
+        self._delimiter = delimiter
+
+    def parse_line(self, line):
+        return list(map(str.strip, line.split(self.delimiter)))
+
+    def __iter__(self):
+        self._iter = iter(self._stream)
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        line = self._iter.__next__()
+        while self.comment and line.startswith(self.comment):
+            line = self._iter.__next__()
+
+        return self.parse_line(line)
+
+
+class Papers(MASAbstract):
+    FIELDS = {"pid": 0,
+              "title_raw": 1,
+              "title_normalized": 2,
+              "year": 3,
+              "date": 4,
+              "doi": 5,
+              "venue_raw": 6,
+              "venue_normalized": 7,
+              "journal_id": 8,
+              "conference_id": 9}
+
+    def __init__(self, stream, delimiter="\t"):
+        super(MASAbstract, self).__init__(stream, delimiter)
+
+    def get_journal_count(self):
+        journals = defaultdict(int)
+        for entry in self:
+            journals[entry[Papers.FIELDS["journal_id"]]] += 1
+
+        return dict(journals)
+
+
+class PaperReferences:
     def __init__(self):
         self.ids = defaultdict(accum)
         self.edges = defaultdict(list)
@@ -60,6 +106,7 @@ if __name__ == "__main__":
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     args = parser.parse_args()
 
-    p = MASParser()
-    p.parse_papers(args.infile)
-    p.to_pajek(args.outfile)
+    p = Papers(args.infile)
+    journal_counts = p.get_journal_count()
+    for jid, count in journal_counts.items():
+        args.outfile.write("{}\t{}\n".format(jid, count))
